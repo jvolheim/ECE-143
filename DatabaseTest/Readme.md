df7365a12edfa2ae7e42dcc83ee68436ab25eb86
jvolheim <81718202+jvolheim@users.noreply.github.com>
2022-11-12 13:09:58 -0800

    Database prelim finished error with score

--- DatabaseTest/Readme.md
+++ DatabaseTest/Readme.md
@@ -1,8 +1,6 @@
 # Current Progress  
     1) Currently able to read in the files and return necessary data into Result.csv    
-    2) ToDo 
-        - Clean up code 
-        - Redo Result database labeling 
-        - Check math on Scores as it seem off   
+    2) ToDo
+        - Check math on Scores as it seem off code in run_wicket()  
 
 Useful resource link on [pandas](https://pandas.pydata.org/pandas-docs/stable/reference/index.html)
--- DatabaseTest/main.py
+++ DatabaseTest/main.py
@@ -3,56 +3,92 @@ import os
 
 
 def run_wickets(df, over):
+    '''
+    # calculate score for each over based on runs, extras and innings
+    :param df:  full dataframe
+    :param over: descriptor of [1,2,3] representing power, middle, and death overs
+    :return: (sum of runs + sum of extras, sum of wickets) for designated over
+    '''
+    # load ball data to parse dataframe on ie1==[0,6.1) 2 == [6.1,16.1) 3 == [16.1,end)
     field = df['ball']
+
+    # switch on over
     if over == 1:
-        A = field[field >= 6.1].index.tolist()
-        field = field.drop(index=A)
-    if over == 2:
-        A = field[field >= 16.1].index.tolist()
-        field = field.drop(index=A)
+        # find index of all balls indexes less than 6.1
         A = field[field < 6.1].index.tolist()
-        field = field.drop(index=A)
-    if over == 3:
-        A = field[field < 16.1].index.tolist()
+        # remove all balls of above index from ball column leaving (balls >= 6.1)
         field = field.drop(index=A)
 
+    elif over == 2:
+
+        # find index of all balls over 16.1
+        A = field[field < 16.1].index.tolist()
+        B = set(A)-set(field[field < 6.1].index.tolist())
+
+        # remove all balls of above index from ball column leaving (balls >= 16.1 and balls < 6.1)
+        field = field.drop(index=B)
+
+    else:
+        # find index of all balls over 16.1
+        A = field[field >= 16.1].index.tolist()
+        # remove all balls of above index from ball column leaving (balls < 16.1)
+        field = field.drop(index=A)
+
+    # All remaining balls in the field are outside the over so find index of them
     A = field[field > 0].index.tolist()
+
+    # load wicket, run and extra data from dataframe
     wick = df['wicket_type']
     run = df['runs_off_bat']
     ex = df['extras']
 
+    # Use indexes based on "A" to remove values from wicket, run and extra data
     wick = wick.drop(index=A).astype(str)
-    wick = wick.apply(lambda x: len(x) > 3) * 1
-
+    wick = wick.apply(lambda x: len(x) > 3) * 1 # convert text to int
     run = run.drop(index=A)
-
     ex = ex.drop(index=A)
 
+    # find sum of all values in each remaining column
     w = wick.sum()
     r = run.sum()
     e = ex.sum()
 
+    # return sum of runs and extras if first column and sum of wickets in second column
     return [[r + e], [w]]
 
 
-def inning(dataf, over):
+def inning(dataf, half_value):
+    '''
+    pull data for each inning of the game
+    :param dataf: full dataframe
+    :param half_value: which inning of the game we are looking at
+    :return:
+    '''
+    # pull inning data
     field = dataf["innings"]
-    A = field[field == over].index.tolist()
+    # find index of the inning data based on first or second inning of game
+    A = field[field != half_value].index.tolist()
+    # drop data from opposite inning
     df = dataf.drop(index=A)
+    # reset index
     df = df.reset_index()
-    r = {}
+    # initiate dictionary to hold result
+    result = {}
 
-    r['team_A'] = [df["batting_team"][0]]
-    r['team_B'] = [df["bowling_team"][0]]
-    r['RiP'], r['WliP'] = run_wickets(df, 1)
-    r['RiM'], r['WliM'] = run_wickets(df, 2)
-    r['RiD'], r['WliD'] = run_wickets(df, 3)
+    # load data into results dictionary
+    result['team_A'] = [df["batting_team"][0]]
+    result['team_B'] = [df["bowling_team"][0]]
+    result['Runs_in_Powerplay'], result['Wickets_lost_in_Powerplay'] = run_wickets(df, 1)
+    result['Runs_in_middle_overs'], result['Wickets_lost_in_middle_overs'] = run_wickets(df, 2)
+    result['Runs_in_Death_overs'], result['Wickets_lost_in_death_overs'] = run_wickets(df, 3)
 
-    r['Winning Score'] = int(r['RiP'][0]) + int(r['WliP'][0]) + \
-                         int(r['RiM'][0]) + int(r['WliM'][0]) + \
-                         int(r['RiD'][0]) + int(r['WliD'][0])
+    # add all overs score to calculate winning score
+    result['Total_Score_A'] = int(result['Runs_in_Powerplay'][0]) + int(result['Wickets_lost_in_Powerplay'][0]) + \
+                         int(result['Runs_in_middle_overs'][0]) + int(result['Wickets_lost_in_middle_overs'][0]) + \
+                         int(result['Runs_in_Death_overs'][0]) + int(result['Wickets_lost_in_death_overs'][0])
 
-    return r
+    # return result dictionary
+    return result
 
 
 def id_info_csv(path):
@@ -105,49 +141,78 @@ def id_csv(path):
     :param path: should be valid path to file
     :return:
     '''
-    r1 = {}
-    # try:
-    dataframe = pd.read_csv(path, usecols=["match_id", "start_date", "venue", "innings",
+    assert isinstance(path,str)
+
+    # initialization of first inning dictionary
+    first_inning = {}
+
+    try:
+        # load data from file
+        dataframe = pd.read_csv(path, usecols=["match_id", "start_date", "venue", "innings",
                                            "ball", "batting_team", "bowling_team", "runs_off_bat",
                                            "extras", "wicket_type"])
 
-    r1['id'] = str(dataframe["match_id"][0])
-    r1['year'] = [int(dataframe['start_date'][0].split('-')[0])]
-    r1['venue'] = [dataframe["venue"][0]]
-    r2 = r1.copy()
+        # retrieve constant data across the two innings
+        first_inning['id'] = str(dataframe["match_id"][0])
+        first_inning['year'] = [int(dataframe['start_date'][0].split('-')[0])]
+        first_inning['venue'] = [dataframe["venue"][0]]
+        second_inning = first_inning.copy()
 
-    r1['id'] = [r1['id'] + 'A']
-    r2['id'] = [r2['id'] + 'B']
+        # Create unique id for each inning
+        first_inning['id'] = [first_inning['id'] + 'A']
+        second_inning['id'] = [second_inning['id'] + 'B']
 
-    inn_1 = inning(dataframe, 1)
-    inn_2 = inning(dataframe, 2)
+        # Get Data from each inning
+        inn_1 = inning(dataframe, 1)
+        inn_2 = inning(dataframe, 2)
 
-    r1.update(inn_1)
-    r2.update(inn_2)
+        # Add all the data together
+        first_inning.update(inn_1)
+        second_inning.update(inn_2)
 
-    del dataframe
-    return r1, r2
-    # except:
-    # print('Error Occurred with path: ' + path)
+        # delete the dataframe
+        del dataframe
+
+        # return data from each inning
+        return first_inning, second_inning
+    except:
+        print('Error Occurred with path: ' + path)
 
 
 if __name__ == '__main__':
-
+    # path to directory
     your_path = './Sample Data'
     files = os.listdir(your_path)
+
+    # create result dataframe
     frame = pd.DataFrame()
+
+    # loop over files in directory
     for file in files:
+
+        # check if not an info file
         if file[-8:-4] != 'info':  # id.csv
 
+            # pull data from id.csv
             fieldA, fieldB = id_csv(your_path + '/' + file)
 
+            # pull data from id_info.csv
             info = id_info_csv(your_path + '/' + file[:-4] + '_info.csv')
 
+            # combine data from both files
             fieldA.update(info)
             fieldB.update(info)
 
+            # just deleting info for ease of reading debugger
+            del info
+
+            # add data to dataframe
             frame = pd.concat([frame, pd.DataFrame(fieldA)])
             frame = pd.concat([frame, pd.DataFrame(fieldB)])
 
-    print(frame)
+            # just deleting info for ease of reading debugger
+            del fieldA
+            del fieldB
+
+    print(frame.head())
     frame.to_csv('Result.csv')
